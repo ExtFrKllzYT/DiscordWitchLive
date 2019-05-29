19807bf51f69514a1bae6c3779adc528d66df0a1
ExtFrKllzYT <49123035+ExtFrKllzYT@users.noreply.github.com>
2019-05-29 11:15:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ExtremeClan4Ever`,"http://twitch.tv/ExtremeClan")
+client.user.setGame(`Extreme-FrKllz + Fortnite`,"http://twitch.tv/ExtremeClan")
 client.user.setStatus("dnd")
 });
 
